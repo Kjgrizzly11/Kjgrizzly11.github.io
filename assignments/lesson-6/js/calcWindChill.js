213faa013d93de4febad777477c0fb83d1122327
Kjgrizzly11 <kjgrizzly11@gmail.com>
2020-06-02 11:45:26 -0600

    commit

---
+++ assignments/lesson-6/js/calcWindChill.js
@@ -0,0 +1,18 @@
+function calcWindChill() {
+
+    let  t = parseFloat(document.getElementById('temp').innerHTML);
+    let w = parseFloat(document.getElementById('wSpeed').innerHTML);
+    console.log(w);
+    console.log(t);
+    
+    if(t <= 50 && w > 3){
+    let windChill = 35.74 + 0.6215 * t -35.75 * Math.pow(w, 0.16) + 0.4275 *t * Math.pow(w, .16);
+    windChill = Math.round(windChill);
+    document.getElementById('windChill').innerHTML = windChill;
+    }
+    else{
+        document.getElementById('windChillDiv').innerHTML = 'N/A';
+    }
+    }
+    
+    calcWindChill();
