e2efd8638c23561a05114ca0daba8008a649a985
Kjgrizzly11 <kjgrizzly11@gmail.com>
2020-06-09 12:13:31 -0600

    commit

---
+++ assignments/lesson-7-part-two/js/formatDate.js
@@ -0,0 +1,17 @@
+function formatDate() {
+    var raw_date = new Date();
+    var daysofWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var dayofWeek = daysofWeek[raw_date.getDay()];
+    var monthsofyear = ['January', 'February','March','April','May','June','July','August','September','October','November','December'];
+    var monthofYear = monthsofyear[raw_date.getMonth()];
+
+    var output = dayofWeek + ', ' + raw_date.getDate() + ' ' +  monthofYear + ' ' + raw_date.getFullYear();
+    document.getElementById('formatDate').innerHTML = output;
+
+    //Display Banner
+    if(daysofWeek == '5'){
+        document.getElementById('banner').style.display = "block";
+    }
+}
+formatDate();
+
